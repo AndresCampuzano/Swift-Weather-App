f1db2627cb9f4f9f8349fe051e26528e30d6cdfb
AndresCampuzano <campuxano88@gmail.com>
2024-05-16 11:56:01 -0500

    Initial Commit

---
+++ SwiftWeatherApp/SwiftWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppApp.swift
+//  SwiftWeatherApp
+//
+//  Created by Andres C G on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
