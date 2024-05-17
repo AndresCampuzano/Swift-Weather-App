0d740c0235ad96185157632ba201ed2d7526794b
AndresCampuzano <campuxano88@gmail.com>
2024-05-17 10:45:59 -0500

    fix env loading at archive file

--- SwiftWeatherApp/Managers/WeatherManager.swift
+++ SwiftWeatherApp/Managers/WeatherManager.swift
@@ -8,35 +8,69 @@
 import Foundation
 import CoreLocation
 
+enum WeatherError: Error, LocalizedError {
+    case missingEnvVariable(String)
+    case invalidURL
+    case networkError(Error)
+    case unexpectedResponse
+    case dataParsingError(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .missingEnvVariable(let variable):
+            return "Missing environment variable: \(variable)"
+        case .invalidURL:
+            return "Invalid URL."
+        case .networkError(let error):
+            return "Network error occurred: \(error.localizedDescription)"
+        case .unexpectedResponse:
+            return "Unexpected response from the server."
+        case .dataParsingError(let error):
+            return "Error parsing data: \(error.localizedDescription)"
+        }
+    }
+}
+
 class WeatherManager {
     let env = ProcessInfo.processInfo.environment
-    var apiKey: String = ""
+    var apiKey: String
     
-    init() {
-        self.apiKey = env["WEATHER_API_KEY"] ?? ""
+    init() throws {
+        let envDict = Bundle.main.infoDictionary?["LSEnvironment"] as! Dictionary<String, String>
+
+        // ENV guide: https://stackoverflow.com/questions/14214741/xcode-environment-variables-not-present-during-archive
+        // and https://vikramios.medium.com/mastering-environment-variables-in-swift-84b27d6ec7aa
+        guard let apiKey = envDict["WEATHER_API_KEY"] else {
+            throw WeatherError.missingEnvVariable("WEATHER_API_KEY")
+        }
+        self.apiKey = apiKey
     }
-    
     
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric") else {
-            fatalError("Missing URL")
+            throw WeatherError.invalidURL
         }
         
         let urlRequest = URLRequest(url: url)
         
-        let (data, response) = try await URLSession.shared.data(for: urlRequest)
-        
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-            fatalError("Error fetching weather data")
+        do {
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw WeatherError.unexpectedResponse
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+                return decodedData
+            } catch {
+                throw WeatherError.dataParsingError(error)
+            }
+        } catch {
+            throw WeatherError.networkError(error)
         }
-        
-        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
-        
-        return decodedData
-        
     }
 }
-
 
 struct ResponseBody: Decodable {
     var coord: CoordinatesResponse
--- SwiftWeatherApp/Views/ContentView.swift
+++ SwiftWeatherApp/Views/ContentView.swift
@@ -10,20 +10,32 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
     @StateObject var locationManager = LocationManager()
-    var weatherManager = WeatherManager()
+    @State var weatherManager: WeatherManager?
     @State var weather: ResponseBody?
+    @State var errorMessage: String?
+    
+    init() {
+        do {
+            _weatherManager = State(initialValue: try WeatherManager())
+        } catch {
+            _weatherManager = State(initialValue: nil)
+            _errorMessage = State(initialValue: "Error initializing WeatherManager: \(error.localizedDescription)")
+        }
+    }
     
     var body: some View {
         VStack {
-            
-            if let location = locationManager.location {
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundStyle(.red)
+            } else if let location = locationManager.location {
                 if let weather = weather {
                     WeatherView(weather: weather)
                 } else {
                     LoadingView()
                         .task {
                             do {
-                                weather = try await WeatherManager().getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                                weather = try await weatherManager?.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
                             } catch {
                                 print("Error getting weather: \(error)")
                             }
@@ -39,7 +51,7 @@ struct ContentView: View {
             }
         }
         .background(colorScheme == .light
-                    ? Color(hue: 0.664, saturation: 0.811, brightness: 0.46) 
+                    ? Color(hue: 0.664, saturation: 0.811, brightness: 0.46)
                     : Color(hue: 0.664, saturation: 0.811, brightness: 0.26)
         )
     }
